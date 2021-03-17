01ba96fc35f9cb2c53bce9ec1d308b2d3e432ee8
anup-chaudhari <anup.chaudhari@nopadvance.com>
2021-03-17 11:35:07 +0530

    added files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebase: {
+  apiKey: "AIzaSyB9hbZQgkBv_EgmTQ_R97MFPGHsUOzJeLQ",
+  authDomain: "my-new-shop-926fc.firebaseapp.com",
+  projectId: "my-new-shop-926fc",
+  storageBucket: "my-new-shop-926fc.appspot.com",
+  messagingSenderId: "262998263760",
+  appId: "1:262998263760:web:478df628441271aec5dbd5",
+  measurementId: "G-KWB69LX1HZ"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebase: {
+  apiKey: "AIzaSyB9hbZQgkBv_EgmTQ_R97MFPGHsUOzJeLQ",
+  authDomain: "my-new-shop-926fc.firebaseapp.com",
+  projectId: "my-new-shop-926fc",
+  storageBucket: "my-new-shop-926fc.appspot.com",
+  messagingSenderId: "262998263760",
+  appId: "1:262998263760:web:478df628441271aec5dbd5",
+  measurementId: "G-KWB69LX1HZ"
+}
+
 };
 
 /*
